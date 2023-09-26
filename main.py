c6b303b7ddbe60df204e107c1caf6a0cfeedeefb
anandabalqis <146094365+anandabalqis@users.noreply.github.com>
2023-09-26 21:06:37 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+meme_dict = {
+            "BRB": "BE RIGHT BACK",
+            "OMG": "OH MY GOD",
+            "GAPTEK": "kurang update terhadap teknologi"
+            "TY": "thankyou"
+            "TBH": "to be honest"
+            "IYKYK": "if you know you know"
+            "RN": "right now"
+            "FR": "for real"
+            "ILY": "i love you"
+            "SALTING": "salah tingkah"
+            "OTW": "on the way"
+            "BTW": "by the way"
+            }
+  word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+  if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+        
